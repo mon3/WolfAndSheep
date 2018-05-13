7ff6bb40c7bc9c8006a3de6b4cdb2b1bec35ffe7
mon3 <seniut.monika@gmail.com>
2018-05-13 16:55:12 +0200

    sheep and wolf movements added

--- Data.hs
+++ Data.hs
@@ -10,22 +10,46 @@ data Point = Point Int Int deriving (Eq,Show)
 
 data Wolf = Wolf Point deriving Show
 data Sheep = Sheep [Point] deriving (Eq,Show)
-data GameState = State Wolf Sheep deriving Show
+data GameState = GameState Wolf Sheep deriving Show
 data Result = WolfWins | SheepWins | Unrecognized deriving Show
 
+possibleBoardX = [0..7]
+possibleBoardY = [0..7]
+wolfMoves = [Point x y | x <- [1,-1], y <- [1,-1]] -- przesunięcia wilka
+sheepMoves = [Point x y | x <- [1,-1], y <- [1]] -- przesunięcia owcy
+
+sheepNumber = [0..3]
+
+xPoint :: Point -> Int
+xPoint (Point x y) = x
+
+yPoint :: Point -> Int
+yPoint (Point x y) = y
+
+moveHero :: Point -> Point -> Point
+moveHero current delta = Point (xPoint(current) + xPoint(delta)) (yPoint(current)+yPoint(delta))
+
+
+moveWolf :: GameState -> Point -> GameState
+moveWolf (GameState (Wolf position) sheep) delta = (GameState (Wolf (moveHero position delta)) sheep)
+
+
+changeSheepPosition :: Sheep -> Int -> Point -> [Point]
+changeSheepPosition (Sheep (position:sheeps)) 0 delta =  ((moveHero position delta):sheeps)
+changeSheepPosition (Sheep (position:sheeps)) idx delta =  (position: (changeSheepPosition (Sheep sheeps) (idx-1) delta))
+
+
+moveSheep :: GameState -> Int -> Point -> GameState
+moveSheep (GameState wolf sheep) idx delta = (GameState wolf (Sheep (changeSheepPosition sheep idx delta)))
 
 
 initialize :: [Point] -> [Point] -> GameState
-initialize wolfStates sheepStates = State wolf sheep
+initialize wolfStates sheepStates = GameState wolf sheep
 	where 
 	wolfState = (wolfStates!!1)
 	wolf = Wolf wolfState
 	sheep = Sheep sheepStates
 									
--- wolf = Wolf () 
--- sheep = Sheep sheepInitStates
--- initGameameState = State wolf sheep
-
 
 
 -- atRandIndex :: [Point]  -> Point  -- note that this is gives itself an IO action
@@ -34,7 +58,14 @@ initialize wolfStates sheepStates = State wolf sheep
 
 -- res = atRandIndex wolfInitStates
 
+
+
+
+
+---------------------------INITIALIZATION---------------------------------------------
 wolfInitStates = [Point x y | y <- [7], x <- [0..7], odd x] 
 sheepInitStates = [Point x y | y <- [0], x <- [0..7], even x]
-main = print(initialize wolfInitStates sheepInitStates)
-
+-- main = print(initialize wolfInitStates sheepInitStates)
+gameState = initialize wolfInitStates sheepInitStates
+-- main = print(moveWolf gameState (Point 1 (-1)))
+main = print(moveSheep gameState 3 (Point 1 (1))) -- owce o indeksach [0;3] poruszaja sie po planszy
