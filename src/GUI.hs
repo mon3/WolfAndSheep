fcd3000af32ca0b061d80a448ac20c8cf9893b16
mikolajczykmk <mikolajczyk.mk@gmail.com>
2018-05-24 20:52:09 +0200

    Add print chessboard. Sketch views

---
+++ src/GUI.hs
@@ -0,0 +1,67 @@
+module GUI where
+import Model
+import System.Console.ANSI
+
+block = "  "
+
+printChessBoard:: GameState -> Int -> Int -> IO ()
+printChessBoard gameState idxCol idxRow = printChess 64 0 0 gameState idxCol idxRow
+                                          where printChess size nrCol nrRow (GameState (Wolf wolf) sheep) col row | mod (nrCol + nrRow) 2 == 0 && mod size 8 /= 1 && size > 0 = do pickToPrint 0 nrCol nrRow wolf sheep col row
+                                                                                                                                                                                   printChess (size - 1) (nrCol + 1) nrRow gameState (col + 6) 4
+                                                                                                                  | mod (nrCol + nrRow) 2 /= 0 && mod size 8 /= 1 && size > 0 = do pickToPrint 1 nrCol nrRow wolf sheep col row
+                                                                                                                                                                                   printChess (size - 1) (nrCol + 1) nrRow gameState (col + 6) 4
+                                                                                                                  | mod (nrCol + nrRow) 2 /= 0 && mod size 8 == 1 && size > 0 = do pickToPrint 1 nrCol nrRow wolf sheep col row
+                                                                                                                                                                                   putStrLn ""
+                                                                                                                                                                                   printChess (size - 1) 0 (nrRow + 1) gameState 1 2
+                                                                                                                  | mod (nrCol + nrRow) 2 == 0 && mod size 8 == 1 && size > 0 = do pickToPrint 0 nrCol nrRow wolf sheep col row
+                                                                                                                                                                                   putStrLn ""
+                                                                                                                                                                                   printChess (size - 1) 0 (nrRow + 1) gameState 1 2
+                                                                                                                  | otherwise = print gameState
+                                                                                                                  where pickToPrint color nrCol nrRow (Point wCol wRow) (Sheep sheepTiles) col row | wCol == nrCol && wRow == nrRow             = do printWolfSquare col row
+                                                                                                                                                                                                   | isSheepTile sheepTiles (Point nrCol nrRow) = do printSheepSquare col row
+                                                                                                                                                                                                   | color == 0                                 = do printWhiteEmptySquare col row
+                                                                                                                                                                                                   | otherwise                                  = do printBlackEmptySquare col row
+                                                                                                                                                                                                   where isSheepTile sheep (Point nrCol nrRow) = length (filter (True ==) ( map (== (Point nrCol nrRow)) sheep)) /= 0
+
+
+
+printWhiteEmptySquare :: Int -> Int ->  IO ()
+printWhiteEmptySquare col row = printColorSquare Vivid Red Dull Black "" col row
+
+printBlackEmptySquare :: Int -> Int -> IO ()
+printBlackEmptySquare col row = printColorSquare Vivid Yellow Vivid White "" col row
+
+printSheepSquare :: Int -> Int -> IO ()
+printSheepSquare col row =  printColorSquare Dull Green Vivid White "\128017 " col row
+--printSheepSquare col row =  printColorSquare Dull Green Vivid White "S " col row
+
+printWolfSquare :: Int -> Int -> IO ()
+printWolfSquare col row =  printColorSquare Vivid Red Vivid White "\128058 " col row
+--printWolfSquare col row =  printColorSquare Vivid Red Vivid White "W " col row
+
+
+printColorSquare:: ColorIntensity -> Color -> ColorIntensity -> Color -> String -> Int -> Int -> IO ()
+printColorSquare fgi fg bgi bg str column row | (length str) == 0 = printSquare "  " column row False
+                                              | otherwise       = printSquare str column row True
+                                              where printSquare str column row bool= do
+                                                                         cursorUpLine row
+                                                                         cursorForward column
+                                                                         setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
+                                                                         putStr (block ++ block ++ block ) -- ++ "\9145")
+                                                                         setSGR [ ]
+                                                                         putStrLn ""
+                                                                         cursorForward column
+                                                                         setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
+                                                                         putStr (block ++ str ++ block ) -- ++ "\9145")
+                                                                         setSGR [ ]
+                                                                         putStrLn ""
+                                                                         cursorForward column
+                                                                         setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
+                                                                         if bool then setSGR [SetConsoleIntensity BoldIntensity,SetUnderlining SingleUnderline]
+                                                                         else setSGR [SetUnderlining NoUnderline]
+                                                                         putStr (block ++ block ++ block ) -- ++ "\9145")
+                                                                         setSGR [ ]
+                                                                         putStrLn ""
+                                                                         putStrLn ""
+
+
---
+++ src/View.hs
@@ -0,0 +1,77 @@
+--module View where
+import Model
+import GUI
+------------------------------------------------------------------------------------------------------------
+-----------------------   START   --------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+sheepInitStates = [Point x y | y <- [0], x <- [0..7], odd x]
+wolf = Wolf (Point 0 7)
+sheep = Sheep sheepInitStates
+initGameState = GameState wolf sheep
+------------------------------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------------------------------
+-----------------------   VIEWS   --------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+start :: GameState -> IO ()
+start state = do
+    putStrLn "Woolf \128058 and Sheep \128017 Game"
+    putStrLn "1. Rozpoczęcie gry"
+    putStrLn "2. Wczytanie gry"
+    putStrLn "9. Zakończenie gry"
+    option <- getLine
+    if (checkOption ["1","2","9"] option) then case option of
+                                                 "1" -> do print ("Rozpoczęcie gry" ++ option)
+                                                           duringGame state
+                                                 "2" -> print ("Wczytaj gre" ++ option)
+                                                 "9" -> do putStrLn "Dziękujemy za grę. Do zobaczenia!"
+                                                           return ()
+      else
+          do putStrLn "Błędnie Wybrana opcja\n"
+             start initGameState
+------------------------------------------------------------------------------------------------------------
+checkOption:: [String] -> String -> Bool
+checkOption [] _ = False
+checkOption (x:xs) opt = if x == opt then True
+                         else checkOption xs opt
+------------------------------------------------------------------------------------------------------------
+duringGame :: GameState -> IO ()
+duringGame state = do
+    printChessBoard state 1 1
+    putStrLn "Wciśnij przycisk aby wykonać jedną z następujących operacji"
+    putStrLn "1. Wykonaj ruch"
+    putStrLn "2. Zapisz grę"
+    putStrLn "3. Zakończ rozgrywkę"
+    option <- getLine
+    if (checkOption ["1","2","3"] option) then case option of
+                                                 "1" -> duringGame state
+                                                 "2" -> actionAfterSavingState state
+                                                 "3" -> start initGameState
+      else
+          do putStrLn "Błędnie Wybrana opcja\n"
+             duringGame state
+
+------------------------------------------------------------------------------------------------------------
+actionAfterSavingState state = do
+    putStrLn "Zapisanie obecnego stanu gry"
+    putStrLn "1. Powrót do gry"
+    putStrLn "2. Zakończ rozgrywkę"
+    option <- getLine
+    if (checkOption ["1","2"] option) then case option of
+                                            "1" -> duringGame state
+                                            "2" -> start initGameState
+      else
+          do putStrLn "Błędnie Wybrana opcja\n"
+             actionAfterSavingState initGameState
+------------------------------------------------------------------------------------------------------------
+endGame = do
+    putStrLn "Wygrał: "
+    putStrLn "Aby powrócić do ekranu startowego wciśnij dowolny przycisk"
+    x <- getChar
+    start initGameState
+------------------------------------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+        start initGameState
